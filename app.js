015be6f4c7362300f3580144b43af1022ca6ea7f
huy227 <huy2272@gmail.com>
2022-09-29 19:52:30 -0400

    Testing commit with gitmoji :rocket: :memo:

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const fs = require('fs');
 
 const files = fs.readdirSync('./');
 console.log(files);
+
+fs.readdir('./', function(err,files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
