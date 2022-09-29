4a97e440660507f791889141a53188ce3dd0e64d
huy227 <huy2272@gmail.com>
2022-09-29 19:58:01 -0400

    Testing commit with gitmoji :rocket: :memo:
    first multi line comment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const files = fs.readdirSync('./');
 console.log(files);
-
+//Asynchronous function
 fs.readdir('./', function(err,files) {
     if (err) console.log('Error', err);
     else console.log('Result', files);
