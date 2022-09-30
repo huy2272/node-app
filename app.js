314d2e8f8c09a4bb7842d22acfb5c1f51657bd94
huy227 <huy2272@gmail.com>
2022-09-29 22:10:48 -0400

    Working with http module to handle http requests

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const Logger = require('./logger');
-const logger = new Logger();
+const http = require('http');
 
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
+const server = http.createServer((req,res)=> {
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 });
 
-logger.log('message');
+
+//Give the listener a port 
+//Every time there is a new connection/request the server raises an event
+server.listen(3000);
+console.log('Listening on port 3000...');
