cb630679bb5ae07ff8d76d0673fddc99b2315bd3
huy227 <huy2272@gmail.com>
2022-10-02 19:41:23 -0400

    Handling post request, the post request is sent using postman

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const courses = [
     {id: 1, name:'course1'},
     {id: 2, name:'course2'},
     {id: 3, name:'course3'},
 ];
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/api/courses', (req,res)  => {
     res.send(courses);
+});
+
+//This automatically assign a new id by taking arraySize() + 1
+//It then sets the new obj name according to the req.body.name
+//After this it pushes it to the array courses
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+
+    //Push the new object into the array
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/courses/:id', (req,res)=> {
